92d56534a3d9d3d4b1d72a094e1a9c3b125891f2
neostoss <ndiakanaomi@yahoo.co.uk>
2023-09-17 20:14:48 +0100

    ami included directly

--- main.tf
+++ main.tf
@@ -38,3 +38,8 @@ resource "aws_instance" "sonarqube_server" {
     Name = "sonarqube"
   }
 }
+
+/*resource "aws_key_pair" "my_key_pair" {
+  key_name   = "my-key-pair"
+  public_key = file("~/.ssh/id_rsa.pub")
+} */
